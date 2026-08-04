ed2cceff0ccbb8a08c2da5deb081345bf2c78a50
Abhyudai Singh <abhichauhan4804@gmail.com>
2026-08-05 03:04:55 +0530

    All routes and database connectivity almost complete.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const MONGO_URL = 'mongodb://localhost:27017/homigobnb';
 
@@ -22,6 +23,7 @@ app.listen(3000, () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 //Index Route
@@ -35,6 +37,20 @@ app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", {listing});
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
 //Create Route
 app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.listing);
@@ -42,11 +58,18 @@ app.post("/listings", async (req, res) => {
   res.redirect(`/listings`);
 });
 
-//Show Route
-app.get("/listings/:id", async (req, res) => {
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
   const { id } = req.params;
   const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", {listing});
+  res.render("listings/edit.ejs", {listing});
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings`);
 });
 
 
