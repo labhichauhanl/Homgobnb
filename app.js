38a5807f5393d9b7b4101333738ffefec9fe75c5
Abhyudai Singh <abhichauhan4804@gmail.com>
2026-08-05 15:40:02 +0530

    Links and boilerplate templates.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const MONGO_URL = 'mongodb://localhost:27017/homigobnb';
 
@@ -24,6 +25,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Index Route
