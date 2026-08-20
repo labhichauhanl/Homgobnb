cab29dd0edc1a30578c91d760bd1954281d8764b
Abhyudai Singh <abhichauhan4804@gmail.com>
2026-08-20 20:16:47 +0530

    Signup page published

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
-const listings = require("./routes/listing");
-const reviews = require("./routes/review");
+const listingRouter = require("./routes/listing");
+const reviewRouter = require("./routes/review");
+const userRouter = require("./routes/user");
 const MONGO_URL = 'mongodb://localhost:27017/homigobnb';
 const session = require("express-session");
 const flash = require("connect-flash");
@@ -70,8 +71,9 @@ app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
